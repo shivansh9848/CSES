90b1fa61972b51accee35197ee4e2c3f658e8575
Shivansh9848 <104058319+shivansh9848@users.noreply.github.com>
2023-01-13 21:03:11 +0530

    commit

--- Introductory Problems/Coin_Piles.cpp
+++ Introductory Problems/Coin_Piles.cpp
@@ -3,10 +3,25 @@ using namespace std;
 
 int main()
 {
-    int t;
+    long long t;
     cin >> t;
     while (t--)
     {
-        
+        long long a, b;
+        cin >> a >> b;
+        int m1 = max(a, b);
+        int m2 = min(a, b);
+        if (m1 <= 2 * m2)
+        {
+            if (a % 3 == 0 && b % 3 == 0)
+                cout << "YES" << endl;
+            else if ((a % 3 == 1 && b % 3 == 2) || (a % 3 == 2 && b % 3 == 1))
+                cout << "YES" << endl;
+            else
+                cout << "NO" << endl;
+
+            continue;
+        }
+        cout << "NO" << endl;
     }
 }
